6442f846680f83e36b75b2d6b0dd430cd91ed382
downplayoficial <downplayoficial@gmail.com>
2025-03-19 17:54:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,54 +8,26 @@ export const config = {
             description: "My Summer Car es un juego de simulación y supervivencia donde deberás construir y mantener tu propio auto mientras sobrevives en la Finlandia rural de los años 90.",
             requirements: {
                 minimum: {
-                    os: "Windows 7",
-                    processor: "Intel Core 2 Duo 2.4GHz",
+                    os: "Windows 7 64 bits",
+                    processor: "Intel Core 2 Duo 2.4GHz o equivalente de AMD",
                     memory: "4 GB RAM",
-                    graphics: "NVIDIA GeForce GTX 460",
+                    graphics: "NVIDIA GeForce GTX 460 o equivalente de AMD",
                     storage: "2 GB"
                 },
                 recommended: {
-                    os: "Windows 10",
-                    processor: "Intel Core i5",
+                    os: "Windows 10 64 bits",
+                    processor: "Intel Core i5 o equivalente de AMD",
                     memory: "8 GB RAM",
-                    graphics: "NVIDIA GeForce GTX 760",
-                    storage: "4 GB"
+                    graphics: "NVIDIA GeForce GTX 760 o equivalente de AMD",
+                    storage: "3 GB"
                 }
             },
             downloads: {
-                mega: "https://mega.nz/your-link-here",
-                mediafire: "https://mediafire.com/your-link-here",
-                drive: "https://drive.google.com/your-link-here"
+                mega: "https://mega.nz/file/q9w1mQ6I#ya23SPHgZorZu0i2y5lAkNgdhVwqMGb4479VCykGCIA",
+                mediafire: "https://www.mediafire.com/file/gfqn5irxu6ehfm9/My1SUmeRC1ArrDownPlay.rar/file",
+                drive: "https://drive.google.com/file/d/1odQk6ch1keA8z5Q-yaqvcauvOtWvLJdl/view?usp=sharinge"
             }
         },
-        {
-            title: "Racing Master",
-            price: "Gratis",
-            image: "data:image/svg+xml,<svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'><rect width='100' height='100' fill='%23e74c3c'/></svg>",
-            downloads: {
-                mega: "#",
-                mediafire: "#",
-                drive: "#"
-            }
-        },
-        {
-            title: "Space Explorer",
-            price: "Gratis",
-            image: "data:image/svg+xml,<svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'><rect width='100' height='100' fill='%232ecc71'/></svg>",
-            downloads: {
-                mega: "#",
-                mediafire: "#",
-                drive: "#"
-            }
-        },
-        {
-            title: "Puzzle Quest",
-            price: "Gratis",
-            image: "data:image/svg+xml,<svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'><rect width='100' height='100' fill='%239b59b6'/></svg>",
-            downloads: {
-                mega: "#",
-                mediafire: "#",
-                drive: "#"
             }
         }
     ]
